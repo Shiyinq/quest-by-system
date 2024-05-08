f55a995ad84028b762c22a3a17744400cce9893d
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-08 20:57:04 +0700

    feat(menu): Update menu command

--- client/telegram-bot/index.js
+++ client/telegram-bot/index.js
@@ -83,7 +83,8 @@ bot.use((ctx, next) => {
 });
 
 bot.command("start", (ctx) => {
-    let menu = `Hello ${ctx.message.from.first_name} do you want get some quest?\n\n/daily\n/weekly\n/monthly\n/side`
+    let menu = `🌟 Hello ${ctx.message.from.first_name}! Welcome to our Bot.\n\n🔍 QUESTS 🔍\n/daily         - Daily Quest\n/weekly      - Weekly Ques\n/monthly    - Monthly Quest\n/side           - Side Quest\n\n📜 HISTORY 📜\n/daily_history       - Daily Quest History\n/weekly_history    - Weekly Quest History\n/monthly_history  - Monthly Quest History\n/side_history         - Side Quest History\n\n📊 STATS 📊\n/stats - View Your Stats\n\n🚀 Ready to embark on your journey? Choose a quest above!`;
+
     ctx.reply(menu)
 })
 

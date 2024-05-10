66f242dbf9b990a65175020c6ee614cb892235f5
Shiyinq <shiyinq.github.io@gmail.com>
2024-05-10 11:30:07 +0700

    feat: Change format datetime

--- client/telegram-bot/src/commands/history/detailHistory.js
+++ client/telegram-bot/src/commands/history/detailHistory.js
@@ -1,6 +1,22 @@
 import { detailQuest } from "../../apis/quests.js";
 
 
+const formatTime = (date) => {
+    if (!date) {
+        return '-'
+    }
+    const dateObj = new Date(date);
+    const year = dateObj.getFullYear();
+    const month = ('0' + (dateObj.getMonth() + 1)).slice(-2);
+    const day = ('0' + dateObj.getDate()).slice(-2);
+    const hours = ('0' + dateObj.getHours()).slice(-2);
+    const minutes = ('0' + dateObj.getMinutes()).slice(-2);
+    const seconds = ('0' + dateObj.getSeconds()).slice(-2);
+
+    return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+}
+
+
 const detailHistory = async (ctx) => {
     let questId = ctx.message.text.substring(1);
 
@@ -11,7 +27,7 @@ const detailHistory = async (ctx) => {
         return
     }
 
-    let metadata = `ID: ${detail.questId}\nTYPE: ${detail.type}\nSTATUS: ${detail.status}\nCREATED AT: ${detail.createdAt}\nACCEPTED AT: ${detail.acceptedAt}`
+    let metadata = `ID: ${detail.questId}\nTYPE: ${detail.type}\nSTATUS: ${detail.status}\n\nCREATED AT: ${formatTime(detail.createdAt)}\nACCEPTED AT: ${formatTime(detail.acceptedAt)}\nSTATUS UPDATED AT: ${formatTime(detail.statusUpdatedAt)}`
     let data = `${metadata}\n\n\nQUEST:\n${detail.quest}`;
 
     let confirmButton = []

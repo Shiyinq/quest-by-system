573b70cf52c801c962c92f1d84ffd61922a4c213
Shiyinq <shiyinq.github.io@gmail.com>
2024-05-11 15:49:56 +0700

    feat: Add parameter status to api user quest history

--- client/telegram-bot/src/apis/users.js
+++ client/telegram-bot/src/apis/users.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { BASE_URL_BACKEND } from "../config/index.js";
 
-export const userQuestHistory = async (userId, type = "all", page = 1, limit = 8) => {
+export const userQuestHistory = async (userId, type = "all", status = null, page = 1, limit = 8) => {
     try {
-        const response = await axios.get(`${BASE_URL_BACKEND}/api/users/${userId}/quests/history?type=${type}&page=${page}&limit=${limit}`);
+        const response = await axios.get(`${BASE_URL_BACKEND}/api/users/${userId}/quests/history?type=${type}&status=${status}&page=${page}&limit=${limit}`);
         return response.data;
     } catch (error) {
         console.log(error.response.status);

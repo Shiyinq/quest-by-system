6782d1829b65d7de15e823178a63355c36b95942
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-13 09:41:35 +0700

    feat: Add new endpoint API to check server and database status

--- src/api.py
+++ src/api.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from src.healthcheck.route import router as healthcheck_router
 from src.quest.route import router as quest_router
 from src.user.route import router as user_router
 
@@ -7,3 +8,4 @@ router = APIRouter()
 
 router.include_router(quest_router, tags=["Quest"])
 router.include_router(user_router, tags=["User"])
+router.include_router(healthcheck_router, tags=["Health Check"])

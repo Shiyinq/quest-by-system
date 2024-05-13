81d48247720a8b5dd47a910a16f61565be2e65c9
Shiyinq <shiyinq.github.io@gmail.com>
2024-05-13 10:08:31 +0700

    feat: Update command ping check dynamic status api and database

--- client/telegram-bot/src/commands/ping.js
+++ client/telegram-bot/src/commands/ping.js
@@ -1,6 +1,18 @@
-const ping = (ctx) => {
-    const status = "STATUS:\n\n🟢 BOT: OK - 🟢 API: OK";
-    ctx.reply(status);
+import { healthCheck } from "../apis/healthcheck.js";
+
+const ping = async (ctx) => {
+    const startTime = Date.now();
+    let status = await healthCheck();
+    const endTime = Date.now();
+    const responseTime = (endTime - startTime) / 1000;
+    const emojiResponseTime = responseTime < 1 ? '⚡️' : '⏱️';
+
+    if (!status) {
+        ctx.reply(`🚨 STATUS 🚨\n\n🟢 BOT: UP\n\n🔴 API: DOWN\n\n🔴 DB: DOWN\n\nResponse Time: ${emojiResponseTime} ${responseTime} seconds`);
+        return;
+    }
+
+    ctx.reply(`🚨 STATUS 🚨\n\n🟢 BOT: UP\n\n🟢 API: ${status.api_status}\n\n🟢 DB: ${status.db_status}\n\nResponse Time: ${emojiResponseTime} ${responseTime} seconds`);
 }
 
-export default (bot) => bot.command("ping", (ctx) => { ping(ctx) });
+export default (bot) => bot.command("ping", async (ctx) => { await ping(ctx) });

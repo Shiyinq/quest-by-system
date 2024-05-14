43da4177e19004cb4539c0b42b861d876d9fcdbe
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-14 21:21:59 +0700

    feat: Add new api userRegister & userDetail

--- client/telegram-bot/src/apis/users.js
+++ client/telegram-bot/src/apis/users.js
@@ -1,6 +1,33 @@
 import axios from "axios";
 import { BASE_URL_BACKEND } from "../config/index.js";
 
+export const userRegister = async (userId, name) => {
+    try {
+        const response = await axios.post(`${BASE_URL_BACKEND}/api/users/register`, {
+            userId: userId,
+            name: name,
+            source: "telegram"
+        })
+        return response.data;
+    } catch (error) {
+        console.log(error.response.status);
+        return false;
+    }
+}
+
+export const userDetail = async (userId) => {
+    try {
+        const response = await axios.get(`${BASE_URL_BACKEND}/api/users/${userId}/detail`);
+        return response.data;
+    } catch (error) {
+        console.log(error.response.status);
+        if (error.response.status == 404) {
+            return 404;
+        }
+        return false;
+    }
+}
+
 export const userQuestHistory = async (userId, type = "all", status = null, page = 1, limit = 8) => {
     try {
         const response = await axios.get(`${BASE_URL_BACKEND}/api/users/${userId}/quests/history?type=${type}&status=${status}&page=${page}&limit=${limit}`);

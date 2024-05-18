2aff29105fcc82242e729e5124c77cb8ebafa98f
Shiyinq <shiyinq.github.io@gmail.com>
2024-05-18 18:46:23 +0700

    feat: Update schema UserCreate & add new schema ResponseIUserDetail

--- src/user/schemas.py
+++ src/user/schemas.py
@@ -1,16 +1,56 @@
 from datetime import datetime
 from typing import List, Union
+from uuid import uuid4
 
-from pydantic import BaseModel, Field
+from passlib.context import CryptContext
+from pydantic import BaseModel, Field, model_validator
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def has_password(password) -> str:
+    return pwd_context.hash(password)
 
 
 class UserCreate(BaseModel):
-    userId: str
+    userId: str = None
+    username: str
     name: str
     goal: str = None
     source: str
+    password: str = None
     createdAt: datetime = Field(default_factory=datetime.now)
     updatedAt: datetime = Field(default_factory=datetime.now)
+
+    @model_validator(mode="after")
+    def update_data(cls, values):
+        if values.userId is None:
+            values.userId = str(uuid4())
+
+        if values.password:
+            values.password = has_password(values.password)
+
+        return values
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "username": "Zer0",
+                "name": "Zero",
+                "source": "web",
+                "password": "Zero123!",
+            }
+        }
+
+
+class ResponseUserDetail(BaseModel):
+    userId: str
+    username: Union[str, None] = None
+    name: Union[str, None] = None
+    goal: Union[str, None] = None
+    source: Union[str, None] = None
+    createdAt: Union[datetime, None] = None
+    updatedAt: Union[datetime, None] = None
 
 
 class ResponseUserCreated(BaseModel):

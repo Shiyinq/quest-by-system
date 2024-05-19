e386f831c104cf3e58d3941aa3514e203c2a3d81
Shiyinq <shiyinq.github.io@gmail.com>
2024-05-19 10:17:27 +0700

    feat: Move router position

--- src/api.py
+++ src/api.py
@@ -6,6 +6,6 @@ from src.user.route import router as user_router
 
 router = APIRouter()
 
-router.include_router(quest_router, tags=["Quest"])
 router.include_router(user_router, tags=["User"])
+router.include_router(quest_router, tags=["Quest"])
 router.include_router(healthcheck_router, tags=["Health Check"])

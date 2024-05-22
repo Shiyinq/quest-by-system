b10b94cda07f1447bfab49bac55c4418dbe14ac8
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-22 16:20:23 +0700

    feat: Add link button more

--- client/web/src/lib/components/quests/CardQuests.svelte
+++ client/web/src/lib/components/quests/CardQuests.svelte
@@ -3,11 +3,12 @@
     export let title = "";
     export let description = "";
     export let quests;
+	export let moreQuestType = "all";
 </script>
 
 
 <div class="dialog">
-    <h2>{title}</h2>
+	<h2>{title}</h2>
     <p>{description}</p>
 	{#if loading}
 		<p>Loading...</p>
@@ -17,6 +18,9 @@
             <li><a class="quest-link" href={'quests/' + questId}>{'/'+questId}</a></li>
         {/each}
     </ul>
+	<div class="dialog-footer">
+		<a class="nb-button blue" href={"/quests/more/" + moreQuestType}>More</a>
+	</div>
 	{/if}
 </div>
 
@@ -27,6 +31,11 @@
 		padding: 20px;
 		border-radius: 10px;
 		margin-bottom: 20px;
+	}
+
+	.dialog-footer {
+		display: flex;
+		justify-content: flex-end;
 	}
 
     .dialog h2 {

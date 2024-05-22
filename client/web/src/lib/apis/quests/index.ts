0267fe1cdc2e15eb3f4cf4c69dbc5ac369dab739
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-22 20:16:45 +0700

    feat: Add API update quest status

--- client/web/src/lib/apis/quests/index.ts
+++ client/web/src/lib/apis/quests/index.ts
@@ -10,3 +10,24 @@ export const getQuestDetail = async (questId: string) => {
 		throw err;
 	}
 };
+
+export const updateQuestStatus = async (questId: string, status: string) => {
+	try {
+		const response = await fetch(`${QUEBYS_API_BASE_URL}/quests/${questId}/status`, {
+			method: 'PUT',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				status: status
+			})
+		});
+
+		const data = await response.json();
+		return data
+	} catch (err) {
+		console.log(err);
+		throw err;
+	}
+}

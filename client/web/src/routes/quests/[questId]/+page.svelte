fec55643dc5472fada17f29a413895f33124f83e
Shiyinq <shiyinq.github.io@gmail.com>
2024-05-22 13:45:24 +0700

    feat: Implement quest detail page to show dynamic data

--- client/web/src/routes/quests/[questId]/+page.svelte
+++ client/web/src/routes/quests/[questId]/+page.svelte
@@ -1,10 +1,29 @@
 <script>
+    import QuestDetail from "$lib/components/quests/QuestDetail.svelte";
     import {page} from "$app/stores";
+    import { getQuestDetail } from "$lib/apis/quests";
+    import LoadingCard from "$lib/components/LoadingCard.svelte";
+    import ReFetchData from "$lib/components/ReFetchData.svelte";
+
     const questId = $page.params.questId;
+
+    let questDetail = getQuestDetail(questId);
+    const reFetchQuestDetail = () => {
+        questDetail = getQuestDetail(questId);
+    }
 </script>
 
 <div class="card-container">
-    <h1>Quest ID: {questId}</h1>
+    {#await questDetail}
+        <LoadingCard />
+    {:then quest}
+        <QuestDetail 
+            quest={quest}
+            showFullContent={true}
+        />
+    {:catch}
+        <ReFetchData actionButton={reFetchQuestDetail}/>
+    {/await}
 </div>
 
 <style>

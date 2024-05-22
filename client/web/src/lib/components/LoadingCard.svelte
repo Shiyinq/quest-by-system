9cb62f77f54b11ed708b6e1c13d3a9798509bcbc
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-22 11:53:18 +0700

    feat: Add new component

---
+++ client/web/src/lib/components/LoadingCard.svelte
@@ -0,0 +1,13 @@
+
+<div class="dialog">
+    <p>Loading...</p>
+</div>
+
+<style>
+    .dialog {
+		width: 480px;
+		padding: 20px;
+		border-radius: 10px;
+		margin-bottom: 20px;
+	}
+</style>
---
+++ client/web/src/lib/components/ReFetchData.svelte
@@ -0,0 +1,28 @@
+<script>
+    const reload = () => {
+        console.log("Reload")
+    }
+
+    export let title = "Failed to get data";
+    export let buttonName = "Reload"
+    export let actionButton = reload;
+</script>
+
+<div class="dialog">
+    <div class="alert error">
+        <p>{title}</p>
+        <button class="nb-button orange rounded" on:click={actionButton}>{buttonName}</button>
+    </div>  
+</div>
+
+<style>
+    .dialog {
+		width: 480px;
+		padding: 20px;
+		border-radius: 10px;
+		margin-bottom: 20px;
+	}
+    button {
+        margin-bottom: 18px;
+    }
+</style>

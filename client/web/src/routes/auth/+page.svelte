64cb0bc6d170ef8bbd521639aaec24ba5adbc093
Shiyinq <shiyinq.github.io@gmail.com>
2024-05-22 17:50:09 +0700

    feat: Add page auth

---
+++ client/web/src/routes/auth/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+
+	let userId = localStorage.getItem('userId') || "";
+
+    const setUserId = (e: any) => {
+        userId = e.target.value;
+    }
+    
+    const login = () => {
+        localStorage.setItem('userId', userId);
+        goto("/")
+    }
+</script>
+
+<div class="card-container">
+    <div class="dialog">
+        <div class="form-title">
+            <h2>LOGIN</h2>
+        </div>
+        <div class="form">
+            <input class="nb-input default" placeholder="ID Telegram" value={userId} on:change={setUserId}/>
+        </div>
+        <div class="form-button">
+            <button class="nb-button blue" on:click={login}>LOGIN</button>
+        </div>
+    </div>
+</div>
+
+<style>
+    .card-container {
+		display: flex;
+        justify-content: center;
+		align-items: center;
+        height: 100vh;
+	}
+
+    .form-title {
+        text-align: center;
+    }
+    .form input {
+        width: 100%;
+        margin-bottom: 12px;
+    }
+
+    .form-button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .form-button button {
+        width: 100%;
+        margin-bottom: 12px;
+    }
+</style>

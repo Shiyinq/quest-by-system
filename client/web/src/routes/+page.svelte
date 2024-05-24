d1ef5f576874c20b686998d43bcfdc7c3bd11837
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-24 11:03:32 +0700

    feat: Use store on home page

--- client/web/src/routes/+page.svelte
+++ client/web/src/routes/+page.svelte
@@ -1,47 +1,66 @@
 <script>
+	import { onMount } from 'svelte';
+	import { dataGeneratedQuests, dataAcceptedQuests } from '$lib/store';
 	import { getUserQuestGenerated, getUserQuestHistory } from '$lib/apis/users';
 	import CardQuests from '$lib/components/quests/CardQuests.svelte';
 	import GenerateQuest from '$lib/components/quests/GenerateQuest.svelte';
 	import LoadingCard from '$lib/components/LoadingCard.svelte';
 	import ReFetchData from '$lib/components/ReFetchData.svelte';
 
-	let getGeneratedQuest = getUserQuestGenerated(localStorage.userId);
-	let getAcceptedQuest = getUserQuestHistory(localStorage.userId, 'all', 'in progress');
-
-	const reFetchGeneratedQuest = () => {
-		getGeneratedQuest = getUserQuestGenerated(localStorage.userId);
+	const fetchGeneratedQuest = async () => {
+		try {
+			dataGeneratedQuests.set('loading');
+			let getGeneratedQuest = await getUserQuestGenerated(localStorage.userId);
+			dataGeneratedQuests.set(getGeneratedQuest);
+		} catch (err) {
+			console.log(err);
+			dataGeneratedQuests.set('error');
+		}
 	};
 
-	const reFetchAcceptedQuest = () => {
-		getAcceptedQuest = getUserQuestHistory(localStorage.userId, 'all', 'in progress');
+	const fetchAcceptedQuest = async () => {
+		try {
+			dataAcceptedQuests.set('loading');
+			let getAcceptedQuest = await getUserQuestHistory(localStorage.userId, 'all', 'in progress');
+			dataAcceptedQuests.set(getAcceptedQuest);
+		} catch (error) {
+			console.log(error);
+			dataAcceptedQuests.set('error');
+		}
 	};
+
+	onMount(() => {
+		fetchGeneratedQuest();
+		fetchAcceptedQuest();
+	});
 </script>
 
 <div class="card-container">
 	<GenerateQuest />
-	{#await getGeneratedQuest}
+
+	{#if $dataGeneratedQuests == 'loading'}
 		<LoadingCard />
-	{:then generatedQuests}
+	{:else if $dataGeneratedQuests == 'error' || !dataGeneratedQuests}
+		<ReFetchData actionButton={fetchGeneratedQuest} />
+	{:else if $dataGeneratedQuests}
 		<CardQuests
 			title="🌟 Generated Quests"
 			description="Quests that have been generated but not yet accepted"
-			quests={generatedQuests}
+			quests={$dataGeneratedQuests}
 		/>
-	{:catch}
-		<ReFetchData actionButton={reFetchGeneratedQuest} />
-	{/await}
+	{/if}
 
-	{#await getAcceptedQuest}
+	{#if $dataAcceptedQuests == 'loading'}
 		<LoadingCard />
-	{:then acceptedQuests}
+	{:else if $dataAcceptedQuests == 'error' || !dataAcceptedQuests}
+		<ReFetchData actionButton={fetchAcceptedQuest} />
+	{:else if $dataAcceptedQuests}
 		<CardQuests
 			title="📜 Recently Accepted Quests"
 			description="Recently received quests"
-			quests={acceptedQuests}
+			quests={$dataAcceptedQuests}
 		/>
-	{:catch}
-		<ReFetchData actionButton={reFetchAcceptedQuest} />
-	{/await}
+	{/if}
 </div>
 
 <style>

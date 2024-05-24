84af2e59f11be6c6bfaa84af5c63f7cb0de9124f
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-24 11:01:59 +0700

    feat: Add new type and variable for generated and accepted quests

--- client/web/src/lib/store/index.ts
+++ client/web/src/lib/store/index.ts
@@ -1,16 +1,46 @@
-import { writable } from 'svelte/store';
+import { writable, type Writable } from 'svelte/store';
 
 const createPersistedStore = (key: string, startValue: string) => {
-    const storedValue = localStorage.getItem(key);
-    const store = writable(storedValue ? JSON.parse(storedValue) : startValue);
+	const storedValue = localStorage.getItem(key);
+	const store = writable(storedValue ? JSON.parse(storedValue) : startValue);
 
-    store.subscribe(value => {
-        if (startValue != '') {
-            localStorage.setItem(key, JSON.stringify(value));
-        }
-    });
+	store.subscribe((value) => {
+		if (startValue != '') {
+			localStorage.setItem(key, JSON.stringify(value));
+		}
+	});
 
-    return store;
-}
+	return store;
+};
 
 export const userId = createPersistedStore('userId', '');
+export const dataGeneratedQuests: Writable<TypeGeneratedQuest | string> = writable();
+export const dataAcceptedQuests: Writable<TypeAcceptedQuest | string> = writable();
+
+export interface TypeGeneratedQuest {
+	metadata: Metadata;
+	data: Quest[];
+}
+
+export interface TypeAcceptedQuest {
+	metadata: Metadata;
+	data: Quest[];
+}
+
+export interface Metadata {
+	page: number;
+	limit: number;
+	prevPage: number | null;
+	nextPage: number | null;
+	totalPage: number;
+}
+
+export interface Quest {
+	questId: string;
+	userId: string;
+	type: string;
+	quest: string;
+	status: string;
+	createdAt: string;
+	acceptedAt: string | null;
+}

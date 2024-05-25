36c93947c301a8d7c3db0cd28c5d6e0ca0dfac1f
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-26 06:56:01 +0700

    style: Formating

--- client/web/src/lib/components/LoadingCard.svelte
+++ client/web/src/lib/components/LoadingCard.svelte
@@ -1,10 +1,10 @@
 <script>
-	import { slide } from "svelte/transition";
+	import { slide } from 'svelte/transition';
 
 	export let text = 'Loading...';
 </script>
 
-<div class="dialog" transition:slide="{{ duration: 1000 }}">
+<div class="dialog" transition:slide={{ duration: 1000 }}>
 	<p>{text}</p>
 </div>
 

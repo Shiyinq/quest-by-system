23843cd64e5d5f572c4945e9485aea582ae84da1
Shiyinq <shiyinq.github.io@gmail.com>
2024-05-25 12:45:34 +0700

    feat: Add custom error page

---
+++ client/web/src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<div class="card-container">
+    <h1>4️⃣0️⃣4️⃣</h1>
+    <p>Page Not Found. Let's get you back on track!</p>
+    <a href="/" class="nb-button blue">🚀 Back to Home</a>
+</div>
+
+<style>
+	.card-container {
+		display: flex;
+        flex-direction: column;
+		justify-content: center;
+		align-items: center;
+        height: 80vh;
+	}
+    h1 {
+        font-size: 70px;
+        margin: 0;
+        padding: 0;
+    }
+</style>

15eece8c6f9fa12191a3a03e25062f9cfe4637b9
Shiyinq <shiyinq.github.io@gmail.com>
2024-05-25 12:52:13 +0700

    feat: Add new util

--- client/web/src/lib/utils/index.ts
+++ client/web/src/lib/utils/index.ts
@@ -10,3 +10,18 @@ export const getLabelNew = (createdAt: string): string => {
 
 	return label;
 };
+
+export const numberToEmoji = (number: number) => {
+	const emojis = ['0️⃣', '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣'];
+	const numString = number.toString();
+	let result = '';
+
+	for (let i = 0; i < numString.length; i++) {
+		const digit = parseInt(numString[i]);
+		if (!isNaN(digit)) {
+			result += emojis[digit];
+		}
+	}
+
+	return result;
+};

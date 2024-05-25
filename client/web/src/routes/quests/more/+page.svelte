5458301fddd175de6c209f9ce4a95a74fc9acd3c
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-25 14:43:52 +0700

    feat: Add functionality to filter quests by status

--- client/web/src/routes/quests/more/+page.svelte
+++ client/web/src/routes/quests/more/+page.svelte
@@ -8,9 +8,11 @@
 	import QuestDetail from '$lib/components/quests/QuestDetail.svelte';
 	import { page } from '$app/stores';
 	import { getUserQuestHistory } from '$lib/apis/users';
+	import { capitalizeWord } from '$lib/utils';
+	import { goto } from '$app/navigation';
 
-	const questType = $page.url.searchParams.get('type') || 'all';
-	const questStatus = $page.url.searchParams.get('status') || 'null';
+	let questType = $page.url.searchParams.get('type') || 'all';
+	let questStatus = $page.url.searchParams.get('status') || 'null';
 
 	let list: any[] = [];
 	let pages = 1;
@@ -39,9 +41,34 @@
 			complete();
 		}
 	};
+
+	const filterButton = async (type: string) => {
+		pages = 1;
+		questStatus = type;
+		const { data } = await getUserQuestHistory(localStorage.userId, questType, questStatus, pages);
+		list = data;
+
+		goto(`${window.location.origin}/quests/more?type=${questType}&status=${questStatus}`);
+	};
 </script>
 
 <div class="card-container">
+	<div class="dialog">
+		<h2>📜 {capitalizeWord(questType)} Quests</h2>
+		<div class="filter-button">
+			<button class="nb-button blue" on:click={async () => await filterButton('null')}>All</button>
+			<button class="nb-button blue" on:click={async () => await filterButton('in progress')}
+				>In progress</button
+			>
+			<button class="nb-button blue" on:click={async () => await filterButton('completed')}
+				>Completed</button
+			>
+			<button class="nb-button blue" on:click={async () => await filterButton('not completed')}
+				>Not Completed</button
+			>
+		</div>
+	</div>
+
 	{#each list as quest}
 		<QuestDetail {quest} showFullContent={false} />
 	{/each}
@@ -55,5 +82,28 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+	}
+
+	.dialog {
+		width: 620px;
+		padding: 20px;
+		border-radius: 10px;
+		margin-bottom: 20px;
+	}
+
+	.dialog h2 {
+		margin: 0 0 10px;
+		color: #0077b6;
+	}
+
+	h2 {
+		font-size: 24px;
+		margin-top: 30px;
+		margin-bottom: 10px;
+		color: #0077b6;
+	}
+
+	button {
+		cursor: pointer;
 	}
 </style>

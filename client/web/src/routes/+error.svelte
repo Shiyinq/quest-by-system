9bacfec3869b3c3dc06314d58668db34cd36302c
Shiyinq <shiyinq.github.io@gmail.com>
2024-05-25 12:53:06 +0700

    feat: Dynamic status code & error message

--- client/web/src/routes/+error.svelte
+++ client/web/src/routes/+error.svelte
@@ -1,20 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { numberToEmoji } from '$lib/utils';
+</script>
+
 <div class="card-container">
-    <h1>4️⃣0️⃣4️⃣</h1>
-    <p>Page Not Found. Let's get you back on track!</p>
-    <a href="/" class="nb-button blue">🚀 Back to Home</a>
+	<h1>{numberToEmoji($page.status)}</h1>
+	<p>Page {$page.error?.message || 'Error'}. Let's get you back on track!</p>
+	<a href="/" class="nb-button blue">🚀 Back to Home</a>
 </div>
 
 <style>
 	.card-container {
 		display: flex;
-        flex-direction: column;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-        height: 80vh;
+		height: 80vh;
 	}
-    h1 {
-        font-size: 70px;
-        margin: 0;
-        padding: 0;
-    }
+	h1 {
+		font-size: 70px;
+		margin: 0;
+		padding: 0;
+	}
 </style>

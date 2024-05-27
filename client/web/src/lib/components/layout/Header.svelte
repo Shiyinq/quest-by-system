a62cd4ee189c21813561e56cf297765ed2c5f476
Shiyinq <shiyinq.github.io@gmail.com>
2024-05-27 10:05:06 +0700

    feat: Update navar and add new fuctional for logout

--- client/web/src/lib/components/layout/Header.svelte
+++ client/web/src/lib/components/layout/Header.svelte
@@ -1,5 +1,6 @@
 <script>
-	import { theme } from '$lib/store';
+	import { goto } from '$app/navigation';
+	import { theme, userId } from '$lib/store';
 	import { slide } from 'svelte/transition';
 
 	let light = true;
@@ -7,24 +8,28 @@
 		light = !light;
 		theme.update((currentTheme) => (currentTheme === 'light' ? 'dark' : 'light'));
 	};
+
+	const logout = () => {
+		localStorage.removeItem('userId');
+		userId.set('');
+		goto('/auth');
+	};
 </script>
 
 <header class="card-container" transition:slide={{ duration: 1000 }}>
 	<div class="dialog">
-		<div></div>
+		<div class="toggle-theme">
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<a href="#" title="Theme" on:click={toggleThemes}>{light ? '🌞' : '🌙'}</a>
+		</div>
 		<div class="navigation">
 			<a href="/">🏠 Home</a>
 			<a href="/quests">🎯 Quests</a>
 			<a href="/profile">👤 Profile</a>
 		</div>
-		<div class="toggle-theme">
-			{#if light}	  
-				<!-- svelte-ignore a11y-invalid-attribute -->
-				<a href="#" on:click={toggleThemes}>🌞</a>
-			{:else}
-				<!-- svelte-ignore a11y-invalid-attribute -->
-				<a href="#" on:click={toggleThemes}>🌙</a>
-			{/if}
+		<div>
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<a href="#" class="logout" on:click={logout}>↪️ Logout</a>
 		</div>
 	</div>
 </header>
@@ -47,5 +52,19 @@
 
 	a:hover {
 		color: #0077b6;
+	}
+
+	.logout {
+		color: red;
+		/* background-color: lightgray; */
+		border: 1px solid lightgray;
+		padding: 5px 10px;
+		text-decoration: none;
+		border-radius: 4px;
+	}
+
+	.logout:hover {
+		color: white;
+		background-color: darkred;
 	}
 </style>

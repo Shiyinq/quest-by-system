8a11f8bf8827042ff41cc3a980e71ba28bc76b36
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-30 17:02:38 +0700

    style: enhance sign-in form styling and add sign-up card

--- client/web/src/routes/auth/+page.svelte
+++ client/web/src/routes/auth/+page.svelte
@@ -2,6 +2,7 @@
 	import { userId } from '$lib/store';
 	import { goto } from '$app/navigation';
 
+	let formSignIn = true;
 	let initialUserId = localStorage.getItem('userId') || '';
 
 	const setUserId = (e: any) => {
@@ -16,20 +17,50 @@
 </script>
 
 <div class="card-container">
-	<div class="dialog">
+	<div class="dialog sign-in" class:hidden={!formSignIn}>
 		<div class="form-title">
-			<h2>LOGIN</h2>
+			<h2>Sign in to QUEBYS</h2>
 		</div>
 		<div class="form">
+			<p class="label-input">ID Telegram</p>
 			<input
 				class="nb-input default"
-				placeholder="ID Telegram"
+				placeholder="Enter Your ID Telegram"
 				value={initialUserId}
 				on:change={setUserId}
 			/>
 		</div>
 		<div class="form-button">
-			<button class="nb-button blue" on:click={login}>LOGIN</button>
+			<button class="nb-button blue" on:click={login}>Sign in</button>
+		</div>
+		<div class="link">
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<p class="link-auth">
+				Don't have an account? <a href="#" on:click={() => (formSignIn = !formSignIn)}>Sign up</a>
+			</p>
+		</div>
+	</div>
+
+	<div class="dialog sign-up" class:hidden={formSignIn}>
+		<div class="form-title">
+			<h2>Sign up to QUEBYS</h2>
+		</div>
+		<div class="form">
+			<p class="label-input">Name</p>
+			<input class="nb-input default" placeholder="Enter Your Full Name" />
+			<p class="label-input">Username</p>
+			<input class="nb-input default" placeholder="Enter Your Username" />
+			<p class="label-input">Password</p>
+			<input class="nb-input default" placeholder="Enter Your Password" type="password" />
+		</div>
+		<div class="form-button">
+			<button class="nb-button blue" on:click={login}>Sign up</button>
+		</div>
+		<div class="link">
+			<!-- svelte-ignore a11y-invalid-attribute -->
+			<p class="link-auth">
+				Already have an account? <a href="#" on:click={() => (formSignIn = !formSignIn)}>Sign in</a>
+			</p>
 		</div>
 	</div>
 </div>
@@ -40,22 +71,45 @@
 		height: 100vh;
 	}
 
+	.dialog {
+		width: 400px;
+	}
+
 	.form-title {
 		text-align: center;
+		margin-bottom: 30px;
 	}
 	.form input {
 		width: 100%;
 		margin-bottom: 12px;
 	}
 
-	.form-button {
+	.form-button,
+	.link {
 		display: flex;
 		justify-content: center;
 		align-items: center;
 	}
 
+	.label-input {
+		font-size: 16px;
+		font-weight: bold;
+		padding: 0px;
+		margin: 8px;
+	}
+
+	.link-auth {
+		font-size: 15px;
+	}
+
 	.form-button button {
 		width: 100%;
 		margin-bottom: 12px;
+		margin-top: 8px;
+		border-radius: 5px;
+	}
+
+	.hidden {
+		display: none;
 	}
 </style>

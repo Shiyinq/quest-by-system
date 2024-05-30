9e3dfd40eac36a7c12ee2b4e0d7c3b6550e2895f
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-30 07:48:45 +0700

    feat: Add component to change quest goal

---
+++ client/web/src/lib/components/profile/QuestGoal.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	let goal = '';
+	const changeGoal = () => {
+		console.log(goal);
+	};
+</script>
+
+<div class="dialog">
+	<h2>Your Goal</h2>
+	<div class="form-goal">
+		<input class="nb-input default input-goal" placeholder="Type your goal" bind:value={goal} />
+		<button class="nb-button blue button-goal" on:click={changeGoal}>SAVE</button>
+	</div>
+</div>
+
+<style>
+	.form-goal {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 3px;
+	}
+	.input-goal {
+		width: 100%;
+	}
+	.button-goal {
+		margin-top: 1px;
+		width: 50%;
+		padding: 11px;
+		border-radius: 5px;
+	}
+</style>

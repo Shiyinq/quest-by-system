9e90aad6967c2f24ed7fd6b224d50693bee99e28
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-30 16:03:52 +0700

    feat: Add splash screen

--- client/web/src/routes/+layout.svelte
+++ client/web/src/routes/+layout.svelte
@@ -3,15 +3,24 @@
 	import { userId } from '$lib/store';
 	import Header from '$lib/components/layout/Header.svelte';
 	import Footer from '$lib/components/layout/Footer.svelte';
+	import SplashScreen from '$lib/components/SplashScreen.svelte';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
 
+	let showSplash = true;
 	onMount(() => {
 		if (!$userId) {
 			goto('/auth');
 		}
+		setTimeout(() => {
+			showSplash = false;
+		}, 3000);
 	});
 </script>
+
+{#if showSplash}
+	<SplashScreen show={showSplash} />
+{/if}
 
 {#if $userId}
 	<Header />

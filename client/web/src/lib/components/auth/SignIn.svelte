14425410e30a2bfef946a6a3fc5a4904248a8e56
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-05-31 16:58:06 +0700

    refactor: Separate components for SignIn and SignUp

---
+++ client/web/src/lib/components/auth/SignIn.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import { userId } from '$lib/store';
+	import { goto } from '$app/navigation';
+
+	let initialUserId = localStorage.getItem('userId') || '';
+
+	const setUserId = (e: any) => {
+		initialUserId = e.target.value;
+	};
+
+	const login = () => {
+		localStorage.setItem('userId', initialUserId);
+		userId.set(initialUserId);
+		goto('/');
+	};
+</script>
+
+<div class="dialog sign-in">
+	<div class="form-title">
+		<h2>Sign in to QUEBYS</h2>
+	</div>
+	<div class="form">
+		<p class="label-input">ID Telegram</p>
+		<input
+			class="nb-input default"
+			placeholder="Enter Your ID Telegram"
+			value={initialUserId}
+			on:change={setUserId}
+		/>
+	</div>
+	<div class="form-button">
+		<button class="nb-button blue" on:click={login}>Sign in</button>
+	</div>
+</div>
+
+<style>
+	.dialog {
+		width: 400px;
+	}
+
+	.form-title {
+		text-align: center;
+		margin-bottom: 30px;
+	}
+	.form input {
+		width: 100%;
+		margin-bottom: 12px;
+	}
+
+	.form-button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.label-input {
+		font-size: 16px;
+		font-weight: bold;
+		padding: 0px;
+		margin: 8px;
+	}
+
+	.form-button button {
+		width: 100%;
+		margin-bottom: 12px;
+		margin-top: 8px;
+		border-radius: 5px;
+	}
+</style>
---
+++ client/web/src/lib/components/auth/SignUp.svelte
@@ -0,0 +1,57 @@
+<script>
+	const signUp = () => {
+		console.log('Sign up');
+	};
+</script>
+
+<div class="dialog sign-up">
+	<div class="form-title">
+		<h2>Sign up to QUEBYS</h2>
+	</div>
+	<div class="form">
+		<p class="label-input">Name</p>
+		<input class="nb-input default" placeholder="Enter Your Full Name" />
+		<p class="label-input">Username</p>
+		<input class="nb-input default" placeholder="Enter Your Username" />
+		<p class="label-input">Password</p>
+		<input class="nb-input default" placeholder="Enter Your Password" type="password" />
+	</div>
+	<div class="form-button">
+		<button class="nb-button blue" on:click={signUp}>Sign up</button>
+	</div>
+</div>
+
+<style>
+	.dialog {
+		width: 400px;
+	}
+
+	.form-title {
+		text-align: center;
+		margin-bottom: 30px;
+	}
+	.form input {
+		width: 100%;
+		margin-bottom: 12px;
+	}
+
+	.form-button {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.label-input {
+		font-size: 16px;
+		font-weight: bold;
+		padding: 0px;
+		margin: 8px;
+	}
+
+	.form-button button {
+		width: 100%;
+		margin-bottom: 12px;
+		margin-top: 8px;
+		border-radius: 5px;
+	}
+</style>

c3df29a30c4e75f8e4dd66bc30e4501dc7c760ba
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-01 13:00:06 +0700

    fix: Fix parsing stored value

--- client/web/src/lib/store/index.ts
+++ client/web/src/lib/store/index.ts
@@ -2,7 +2,16 @@ import { writable, type Writable } from 'svelte/store';
 
 const createPersistedStore = (key: string, startValue: string) => {
 	const storedValue = localStorage.getItem(key);
-	const store = writable(storedValue ? JSON.parse(storedValue) : startValue);
+
+	let parsedValue;
+	try {
+		parsedValue = storedValue ? JSON.parse(storedValue) : startValue;
+	} catch (e) {
+		console.error('Error parsing stored value: ', e);
+		parsedValue = storedValue;
+	}
+
+	const store = writable(parsedValue);
 
 	store.subscribe((value) => {
 		if (startValue != '') {

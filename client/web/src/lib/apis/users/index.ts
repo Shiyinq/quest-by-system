d7b5c4054600e1bdc1cdca402149c2a282df4d7f
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-06-01 09:34:15 +0700

    refactor: Remove try catch

--- client/web/src/lib/apis/users/index.ts
+++ client/web/src/lib/apis/users/index.ts
@@ -1,57 +1,42 @@
 import { QUEBYS_API_BASE_URL } from '$lib/constants';
 
 export const userRegister = async (name: string, username: string, password: string) => {
-	try {
-		const response = await fetch(`${QUEBYS_API_BASE_URL}/users/register`, {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				name: name,
-				username: username,
-				password: password,
-				source: 'web'
-			})
-		});
-		const data = await response.json();
-		return data;
-	} catch (err) {
-		console.log(err);
-		throw err;
-	}
+	const response = await fetch(`${QUEBYS_API_BASE_URL}/users/register`, {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			name: name,
+			username: username,
+			password: password,
+			source: 'web'
+		})
+	});
+	if (!response.ok) await response.json();
+	return await response.json();
 };
 
 export const getUserDetail = async (userId: string) => {
-	try {
-		const response = await fetch(`${QUEBYS_API_BASE_URL}/users/${userId}/detail`);
-		const data = await response.json();
-		return data;
-	} catch (err) {
-		console.log(err);
-		throw err;
-	}
+	const response = await fetch(`${QUEBYS_API_BASE_URL}/users/${userId}/detail`);
+	if (!response.ok) throw await response.json();
+	return await response.json();
 };
 
 export const setUserGoal = async (userId: string, goal: string) => {
-	try {
-		const response = await fetch(`${QUEBYS_API_BASE_URL}/users/${userId}/goal`, {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				goal: goal
-			})
-		});
-		const data = await response.json();
-		return data;
-	} catch (err) {
-		console.log(err);
-		throw err;
-	}
+	const response = await fetch(`${QUEBYS_API_BASE_URL}/users/${userId}/goal`, {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			goal: goal
+		})
+	});
+	if (!response.ok) throw await response.json();
+	return await response.json();
 };
 
 export const getUserQuestGenerated = async (
@@ -59,16 +44,11 @@ export const getUserQuestGenerated = async (
 	page: number = 1,
 	limit: number = 5
 ) => {
-	try {
-		const response = await fetch(
-			`${QUEBYS_API_BASE_URL}/users/${userId}/quests/generated?page=${page}&limit=${limit}`
-		);
-		const data = await response.json();
-		return data;
-	} catch (err) {
-		console.log(err);
-		throw err;
-	}
+	const response = await fetch(
+		`${QUEBYS_API_BASE_URL}/users/${userId}/quests/generated?page=${page}&limit=${limit}`
+	);
+	if (!response.ok) throw await response.json();
+	return await response.json();
 };
 
 export const getUserQuestHistory = async (
@@ -78,25 +58,15 @@ export const getUserQuestHistory = async (
 	page: number = 1,
 	limit: number = 5
 ) => {
-	try {
-		const response = await fetch(
-			`${QUEBYS_API_BASE_URL}/users/${userId}/quests/history?type=${type}&status=${status}&page=${page}&limit=${limit}`
-		);
-		const data = await response.json();
-		return data;
-	} catch (err) {
-		console.log(err);
-		throw err;
-	}
+	const response = await fetch(
+		`${QUEBYS_API_BASE_URL}/users/${userId}/quests/history?type=${type}&status=${status}&page=${page}&limit=${limit}`
+	);
+	if (!response.ok) throw await response.json();
+	return await response.json();
 };
 
 export const getUserQuestStats = async (userId: string) => {
-	try {
-		const response = await fetch(`${QUEBYS_API_BASE_URL}/users/${userId}/quests/stats`);
-		const data = await response.json();
-		return data;
-	} catch (err) {
-		console.log(err);
-		throw err;
-	}
+	const response = await fetch(`${QUEBYS_API_BASE_URL}/users/${userId}/quests/stats`);
+	if (!response.ok) throw await response.json();
+	return await response.json();
 };

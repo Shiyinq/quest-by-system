1c2f92d22fe74b6272db5cc2f4ddbde441f60db7
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-06-02 18:44:34 +0700

    refactor: Replace indirect function calls with direct calls

--- client/web/src/routes/+page.server.ts
+++ client/web/src/routes/+page.server.ts
@@ -1,31 +1,11 @@
 import { getUserQuestGenerated, getUserQuestHistory } from '$lib/apis/users';
 
-const fetchGeneratedQuest = async (userId: string) => {
-	try {
-		const getGeneratedQuest = await getUserQuestGenerated(userId);
-		return getGeneratedQuest;
-	} catch (error) {
-		console.log(error);
-		throw error;
-	}
-};
-
-const fetchAcceptedQuest = async (userId: string) => {
-	try {
-		const getAcceptedQuest = await getUserQuestHistory(userId, 'all', 'in progress');
-		return getAcceptedQuest;
-	} catch (error) {
-		console.log(error);
-		throw error;
-	}
-};
-
 export const load = async ({ cookies }) => {
 	const userId = JSON.parse(cookies.get('userId') || '');
 	try {
 		const [generatedQuest, acceptedQuest] = await Promise.all([
-			fetchGeneratedQuest(userId),
-			fetchAcceptedQuest(userId)
+			getUserQuestGenerated(userId),
+			getUserQuestHistory(userId, 'all', 'in progress')
 		]);
 
 		return {

60679e6cc820f7c2e11090388462b2897aee2dc0
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-05 11:17:22 +0700

    feat: Import base url from .env

--- client/web/src/lib/constants.ts
+++ client/web/src/lib/constants.ts
@@ -1,1 +1,3 @@
-export const QUEBYS_API_BASE_URL = 'http://localhost:5000/api';
+import { PUBLIC_QUEBYS_API_BASE_URL } from '$env/static/public';
+
+export const QUEBYS_API_BASE_URL =PUBLIC_QUEBYS_API_BASE_URL

414b74050707ee7c752a220bf4371f7db9a0a37f
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-06-05 08:45:43 +0700

    fix: Handle erorr document

--- client/web/src/lib/store/index.ts
+++ client/web/src/lib/store/index.ts
@@ -2,43 +2,55 @@ import cookie from 'cookie';
 import { writable, type Writable } from 'svelte/store';
 
 const createPersistedStore = (key: string, startValue: string) => {
-	const cookies = cookie.parse(document.cookie);
-	const storedValue = cookies[key];
-
-	let parsedValue;
-	try {
-		parsedValue = storedValue ? JSON.parse(storedValue) : startValue;
-	} catch (e) {
-		console.error('Error parsing stored value: ', e);
-		parsedValue = storedValue;
-	}
-
-	const store = writable(parsedValue);
-
-	store.subscribe((value) => {
-		if (value !== '') {
-			document.cookie = cookie.serialize(key, JSON.stringify(value), {
-				path: '/',
-				maxAge: 365 * 24 * 60 * 60
+		let parsedValue = startValue;
+	
+		if (typeof document !== 'undefined') {
+			const cookies = cookie.parse(document.cookie);
+			const storedValue = cookies[key];
+	
+			try {
+				parsedValue = storedValue ? JSON.parse(storedValue) : startValue;
+			} catch (e) {
+				console.error('Error parsing stored value: ', e);
+				parsedValue = startValue;
+			}
+		}
+	
+		const store = writable(parsedValue);
+	
+		if (typeof document !== 'undefined') {
+			store.subscribe((value) => {
+				if (value !== '') {
+					document.cookie = cookie.serialize(key, JSON.stringify(value), {
+						path: '/',
+						maxAge: 365 * 24 * 60 * 60
+					});
+				}
 			});
 		}
-	});
-
-	return store;
+	
+		return store;
 };
 
 const toggleTheme = () => {
-	const cookies = cookie.parse(document.cookie);
-	const initialTheme = cookies['theme'] || 'light';
+	let initialTheme = 'light';
+
+	if (typeof document !== 'undefined') {
+		const cookies = cookie.parse(document.cookie);
+		initialTheme = cookies['theme'] || 'light';
+	}
+
 	const theme = writable(initialTheme);
 
-	theme.subscribe((value) => {
-		document.cookie = cookie.serialize('theme', value, {
-			path: '/',
-			maxAge: 365 * 24 * 60 * 60
+	if (typeof document !== 'undefined') {
+		theme.subscribe((value) => {
+			document.cookie = cookie.serialize('theme', value, {
+				path: '/',
+				maxAge: 365 * 24 * 60 * 60
+			});
+			document.documentElement.setAttribute('data-theme', value);
 		});
-		document.documentElement.setAttribute('data-theme', value);
-	});
+	}
 
 	return theme;
 };

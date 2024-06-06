86d55357d05941feaec0fcd97014fea7c60ea4e3
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-06-06 08:52:13 +0700

    feat: add new Sign In API

--- src/auth/route.py
+++ src/auth/route.py
@@ -1,14 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 
 from src.auth import service
-from src.auth.schemas import UserCreate
+from src.auth.schemas import Token, UserSignUp
 from src.schemas import GeneralResponse
 
 router = APIRouter()
 
 
 @router.post("/auth/sign-up", status_code=201, response_model=GeneralResponse)
-async def sign_up_user(data: UserCreate):
+async def sign_up_user(data: UserSignUp):
     """Sign up User"""
     user = await service.sign_up(data)
     return user
+
+
+@router.post("/auth/sign-in", status_code=200, response_model=Token)
+async def sign_up_user(form_data: OAuth2PasswordRequestForm = Depends()):
+    """Sign in User"""
+    user = await service.authenticate_user(form_data.username, form_data.password)
+    access_token = service.create_access_token(data={"sub": user.userId})
+
+    return access_token

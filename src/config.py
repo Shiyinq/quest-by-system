7aaff33a17d9b4b12c317ec1eea6f9d430891c37
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-06-06 08:43:26 +0700

    feat: Add config.py

---
+++ src/config.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+
+class Config:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        self.mongo_uri = os.getenv("MONGODB_URI")
+        self.secret_key = os.getenv("SECRET_KEY")
+        self.algorithm = os.getenv("ALGORITHM")
+        self.access_token_expire_minutes = os.getenv("TOKEN_EXPIRE")
+
+
+config = Config()

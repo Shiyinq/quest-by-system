9b63418baceabcd39e581a024ab322b097718e81
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-06-08 11:24:56 +0700

    refactor: separate token validation and user data retrieval

--- src/dependencies.py
+++ src/dependencies.py
@@ -9,16 +9,18 @@ from src.config import config
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/sign-in")
 
-
-async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserId:
+async def validate_token(token: str = Depends(oauth2_scheme)) -> str:
     try:
         payload = jwt.decode(token, config.secret_key, algorithms=[config.algorithm])
         user_id: str = payload.get("sub")
         if user_id is None:
             raise JwtTokenError()
-    except JWTError:
+        return user_id
+    except JWTError as je:
+        print(je)
         raise JwtTokenError()
 
+async def get_current_user(user_id: str = Depends(validate_token)) -> UserId:
     user = await get_user(user_id)
     if user is None:
         raise JwtTokenError()

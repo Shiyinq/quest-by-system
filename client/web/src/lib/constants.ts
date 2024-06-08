28da30bc0c78224c7f8beeef92dc5b04752cc1ea
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-06-08 10:58:03 +0700

    style: formating

--- client/web/src/lib/constants.ts
+++ client/web/src/lib/constants.ts
@@ -1,3 +1,3 @@
 import { PUBLIC_QUEBYS_API_BASE_URL } from '$env/static/public';
 
-export const QUEBYS_API_BASE_URL =PUBLIC_QUEBYS_API_BASE_URL
+export const QUEBYS_API_BASE_URL = PUBLIC_QUEBYS_API_BASE_URL;

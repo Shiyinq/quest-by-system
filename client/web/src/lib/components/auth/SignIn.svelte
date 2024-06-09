0f798c39c728a310ff04ac855ecb22243c437b10
Shiyinq <shiyinq.github.io@gmail.com>
2024-06-09 14:35:23 +0700

    feat: set userId and token after successful sign in

--- client/web/src/lib/components/auth/SignIn.svelte
+++ client/web/src/lib/components/auth/SignIn.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import { goto } from '$app/navigation';
+	import { token, userId } from '$lib/store';
+
+	import { jwtDecode } from 'jwt-decode';
 	import { Toaster, toast } from 'svelte-sonner';
 
 	export let form: any;
@@ -15,7 +18,12 @@
 
 	$: if (form) {
 		if (form?.status) {
+			const { sub } = jwtDecode(form?.access_token);
 			toast.success(form?.message);
+
+			userId.set(sub ? sub : '');
+			token.set(form?.access_token);
+
 			goto('/');
 		} else {
 			toast.error(form?.message);

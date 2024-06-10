9b815f901f2484ed46c4bdbb1ab671f6021f3627
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-06-10 21:08:38 +0700

    fix: change expire token to timezone utc

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 from jose import jwt
 from passlib.context import CryptContext
@@ -35,9 +35,9 @@ def get_password_hash(password) -> str:
 def create_access_token(data: dict, expires_delta: timedelta = None) -> Token:
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.now() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now() + timedelta(minutes=60)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=60)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, config.secret_key, algorithm=config.algorithm)
     return {

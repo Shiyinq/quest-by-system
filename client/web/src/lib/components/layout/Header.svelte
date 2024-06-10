c05997dd590ff54b9fa3f5a1ff5ac24af424e9fd
Shiyinq <25049562+Shiyinq@users.noreply.github.com>
2024-06-10 21:06:12 +0700

    feat: remove userId

--- client/web/src/lib/components/layout/Header.svelte
+++ client/web/src/lib/components/layout/Header.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import cookie from 'cookie';
 	import { goto } from '$app/navigation';
-	import { theme, userId, activeMenu, token } from '$lib/store';
+	import { theme, activeMenu, token } from '$lib/store';
 	import { slide } from 'svelte/transition';
 
 	let light = true;
@@ -11,20 +11,12 @@
 	};
 
 	const logout = () => {
-		const expiredUserId = cookie.serialize('userId', '', {
-			path: '/',
-			maxAge: -1
-		});
-
 		const expiredToken = cookie.serialize('token', '', {
 			path: '/',
 			maxAge: -1
 		});
 
-		document.cookie = expiredUserId;
 		document.cookie = expiredToken;
-
-		userId.set('');
 		token.set('');
 
 		goto('/auth');

29c759f1eb07b1627047432f2b071691d2cfd9cc
Shiyinq <shiyinq.github.io@gmail.com>
2024-07-07 16:04:34 +0700

    feat: handle client and server side base url

--- client/web/src/lib/constants.ts
+++ client/web/src/lib/constants.ts
@@ -1,3 +1,4 @@
-import { PUBLIC_QUEBYS_API_BASE_URL } from '$env/static/public';
+import { browser } from '$app/environment';
+import { PUBLIC_CLIENT_SIDE_QUEBYS_API_BASE_URL, PUBLIC_SERVER_SIDE_QUEBYS_API_BASE_URL } from '$env/static/public';
 
-export const QUEBYS_API_BASE_URL = PUBLIC_QUEBYS_API_BASE_URL;
+export const QUEBYS_API_BASE_URL = browser ? PUBLIC_CLIENT_SIDE_QUEBYS_API_BASE_URL : PUBLIC_SERVER_SIDE_QUEBYS_API_BASE_URL;
